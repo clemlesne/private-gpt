89d1134c9744f9c2805bcc0bc40a01890f3ccdab
Clémence Lesné <clemence@lesne.pro>
2023-06-29 20:45:07 +0200

    UI: Add a 2nd active button on the UI

--- src/conversation-ui/src/Conversations.jsx
+++ src/conversation-ui/src/Conversations.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function Conversations({ conversations, selectedConversation, setSelectedConversation, conversationLoading }) {
   return (
     <div className="conversations">
-      <Button onClick={() => setSelectedConversation(null)} text="New chat" emoji="+" disabled={selectedConversation == null} />
+      <Button onClick={() => setSelectedConversation(null)} text="New chat" emoji="+" disabled={selectedConversation == null} active={true} />
       <p>Conversation history:</p>
       {conversations.map((conversation) => (
         <Button key={conversation.id} onClick={() => setSelectedConversation(conversation.id)} text={conversation.title ? conversation.title : "New chat"} disabled={conversation.id == selectedConversation} loading={conversation.id == selectedConversation && conversationLoading} />
