4aa653f367b37ba9bb8acb17b8576aa54d5b67c6
Clémence Lesné <clemence@lesne.pro>
2023-07-10 19:52:26 +0200

    Perf: Make higher amount of retries but retry more slowly

--- src/conversation-ui/src/Utils.jsx
+++ src/conversation-ui/src/Utils.jsx
@@ -7,8 +7,8 @@ const client = axios.create({
   baseURL: 'http://127.0.0.1:8081',
 });
 axiosRetry(client, {
-  retries: 5, // 5 retries + 1 attempt = 6 requests
-  retryDelay: axiosRetry.exponentialDelay,
+  retries: 11, // 11 retries + 1 attempt = 12 requests
+  retryDelay: (retryCount, err) => axiosRetry.exponentialDelay(retryCount, err, 250),
   shouldResetTimeout: true,
   retryCondition: (err) => {
     return axiosRetry.isNetworkOrIdempotentRequestError(err) || (err.response?.status == 429);
